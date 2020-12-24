38ce426aaf5bc53f9173ed2ad39052a52a7553a3
root <zc150622@gmail.com>
2020-12-24 17:57:22 +0800

    扩展缓存实现，增加redis实现方式

---
+++ src/main/java/com/spang/snakerflow/cache/RedisCache.java
@@ -0,0 +1,42 @@
+package com.spang.snakerflow.cache;
+
+import org.snaker.engine.cache.Cache;
+import org.snaker.engine.cache.CacheException;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author zhao.cheng
+ * @date 2020/12/24 9:47
+ */
+public class RedisCache<K, V> implements Cache<K, V> {
+
+    private final RedisTemplate<K, V> redisTemplate;
+
+    public RedisCache(RedisTemplate<K, V> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+
+    @Override
+    public V get(K key) throws CacheException {
+       return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public V put(K key, V value) throws CacheException {
+        redisTemplate.opsForValue().set(key, value);
+        return value;
+    }
+
+    @Override
+    public V remove(K key) throws CacheException {
+        V v = get(key);
+        redisTemplate.delete(key);
+        return v;
+    }
+
+    @Override
+    public void clear() throws CacheException {
+
+    }
+}
--- src/main/java/com/spang/snakerflow/cache/SnakerRedisCacheManager.java
+++ src/main/java/com/spang/snakerflow/cache/SnakerRedisCacheManager.java
@@ -3,28 +3,53 @@ package com.spang.snakerflow.cache;
 import org.snaker.engine.cache.Cache;
 import org.snaker.engine.cache.CacheException;
 import org.snaker.engine.cache.CacheManager;
+import org.snaker.engine.cache.memory.MemoryCache;
+import org.snaker.engine.helper.StringHelper;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * TODO redis实现cache
  * @author zhao.cheng
  */
-public class SnakerRedisCacheManager<K,V> implements CacheManager {
+@SuppressWarnings("all")
+public class SnakerRedisCacheManager<K, V> implements CacheManager {
 
-    private final RedisTemplate<String, Cache<K,V>> redisTemplate;
+    private final RedisTemplate<K, V> redisTemplate;
+    private final ConcurrentMap<String, Cache> caches;
 
-    public SnakerRedisCacheManager(RedisTemplate<String, Cache<K, V>> redisTemplate) {
+    public SnakerRedisCacheManager(RedisTemplate<K, V> redisTemplate) {
+        this.caches = new ConcurrentHashMap<String, Cache>();
         this.redisTemplate = redisTemplate;
     }
 
 
     @Override
     public <K, V> Cache<K, V> getCache(String name) throws CacheException {
-          return null;
+        if(StringHelper.isEmpty(name)) {
+            throw new IllegalArgumentException("Cache名称不能为空.");
+        }
+        Cache cache;
+        cache = caches.get(name);
+
+        if (cache == null) {
+            cache = new RedisCache(redisTemplate);
+            Cache existing = caches.putIfAbsent(name, cache);
+            if (existing != null) {
+                cache = existing;
+            }
+        }
+
+        return cache;
     }
 
     @Override
     public void destroy() throws CacheException {
-
+        while (!caches.isEmpty()) {
+            caches.clear();
+        }
     }
 }
